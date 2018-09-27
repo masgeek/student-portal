9cb8c2c4b8e4320c6f759f27c3712af8cf19aff5
Sammy Barasa <barsamms@gmail.com>
2018-09-27 12:05:33 +0300

    Updated action uRL

--- card/callback.php
+++ card/callback.php
@@ -3,16 +3,14 @@ $root_dir = dirname(dirname(__FILE__));
 
 require_once $root_dir . '/vendor/autoload.php';
 
-
+$postData = $_POST;
 // Tell log4php to use our configuration file.
-Logger::configure($root_dir.'/config/config.xml');
+Logger::configure($root_dir . '/config/config.xml');
 
-$callbackParams = serialize($_POST);
+$callbackParams = serialize($postData);
 
 // Fetch a logger, it will inherit settings from the root logger
 $log = Logger::getLogger('myLogger');
 
 // Start logging
-$log->info($_POST);
-
-var_dump($_REQUEST);
+$log->info($postData);

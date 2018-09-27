b73354a54b8d4c35fa5e3a4d6a78ab24f1db0fa8
Sammy Barasa <barsamms@gmail.com>
2018-09-27 10:53:40 +0300

    Updated action uRL

--- card/index.php
+++ card/index.php
@@ -27,15 +27,14 @@ $signedField = sign($fieldsToSign);
 </div>
 
 <div class="container">
-    <!--<form id="payment_form" action="payment_confirmation.php" method="POST"> -->
-    <form id="payment_form" action="https://testsecureacceptance.cybersource.com/pay" method="POST">
+    <form id="payment_form" action="payment_confirmation.php" method="POST">
         <input type="hidden" name="access_key" value="<?= $accessKey ?>">
         <input type="hidden" name="profile_id" value="<?= $profileID ?>">
         <input type="hidden" name="transaction_uuid" value="<?= $transactionID ?>">
         <input type="hidden" name="signed_field_names"
                value="access_key,profile_id,transaction_uuid,signed_field_names,unsigned_field_names,signed_date_time,locale,transaction_type,reference_number,amount,currency">
 
-        <input type="text" name="signature" id="signature" value="<?= $signedField ?>" class="form-control">
+        <input type="text" name="s" id="signatures" value="<?= $signedField ?>" class="form-control">
         <input type="hidden" name="unsigned_field_names">
         <input type="hidden" name="signed_date_time" value="<?= gmdate("Y-m-d\TH:i:s\Z"); ?>">
         <input type="hidden" name="locale" value="en">
